b7fc209deb5bc5ff4145b446d1c4dfdd91dd65e2
Aaron Lab <aaronlab.net@gmail.com>
2020-07-20 15:54:32 +0900

    TableExercise

--- Table/Table/AddViewController.swift
+++ Table/Table/AddViewController.swift
@@ -8,20 +8,65 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let PICKER_VIEW_HEIGHT:CGFloat = 70
+    
+    let MAX_ARRAY_NUM = 3
+    let PICKER_VIEW_COMUMN = 1
+    var fileName = ""
+    var imageArray = [UIImage?]()
+    var imageFileName = ["cart.png", "clock.png", "pencil.png"]
+    
 
     @IBOutlet var tfAddItem: UITextField!
+    @IBOutlet var pickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 0 ..< 3 {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        fileName = imageFileName[0]
 
     }
     
     // 새 목록 추가하기
     @IBAction func btnAddItem(_ sender: UIButton) {
         items.append(tfAddItem.text!)
-        itemsImageFile.append("clock.png")
+        itemsImageFile.append(fileName)
         tfAddItem.text = ""
         _ = navigationController?.popViewController(animated: true)
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return itemsImageFile[row]
+//    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        fileName = imageFileName[row]
+    }
+    
 }
