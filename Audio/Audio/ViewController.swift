bf3951a753f6ec9119a04b71b0b2aefd3d7068f2
Aaron Lab <aaronlab.net@gmail.com>
2020-07-20 17:48:26 +0900

    AudioPlayer

---
+++ Audio/Audio/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Audio
+//
+//  Created by Aaron Lee on 7/20/20.
+//  Copyright © 2020 Aaron Lee. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    let MAX_VOLUME: Float = 10.0
+    
+    var audioPlayer: AVAudioPlayer!
+    var audioFile: URL!
+    var progressTimer: Timer!
+    
+    let timePlayerSelector:Selector = #selector(ViewController.updatePlayTime)
+
+    @IBOutlet var pvProgressPlay: UIProgressView!
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblEndTime: UILabel!
+    @IBOutlet var btnPlay: UIButton!
+    @IBOutlet var btnPause: UIButton!
+    @IBOutlet var btnStop: UIButton!
+    @IBOutlet var slVolume: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        audioFile = Bundle.main.url(forResource: "Sicilian_Breeze", withExtension: "mp3")
+        
+        initPlay()
+    }
+    
+    func initPlay() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile)
+        } catch let error as NSError {
+            print("Error-initPlay: \(error)")
+        }
+        
+        // init audio play values
+        slVolume.maximumValue = MAX_VOLUME
+        slVolume.value = 5
+        pvProgressPlay.progress = 0
+        
+        // init player
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        audioPlayer.volume = slVolume.value
+        lblEndTime.text = convertNSTimeInterval2String(audioPlayer.duration)
+        lblCurrentTime.text = convertNSTimeInterval2String(0)
+        
+        // init btns
+        setPlayButtons(true, false, false)
+    }
+    
+    func setPlayButtons(_ play: Bool, _ pause: Bool, _ stop: Bool) {
+        btnPlay.isEnabled = play
+        btnPause.isEnabled = pause
+        btnStop.isEnabled = stop
+    }
+    
+    func convertNSTimeInterval2String(_ time: TimeInterval) -> String {
+        let min = Int(time/60)
+        let sec = Int(time.truncatingRemainder(dividingBy: 60))
+        let strTime = String(format: "%02d:%02d", min, sec)
+        return strTime
+    }
+
+    @IBAction func btnPlayAudio(_ sender: UIButton) {
+        audioPlayer.play()
+        setPlayButtons(false, true, true)
+        progressTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: timePlayerSelector, userInfo: nil, repeats: true)
+    }
+    
+    @objc func updatePlayTime() {
+        lblCurrentTime.text = convertNSTimeInterval2String(audioPlayer.currentTime)
+        pvProgressPlay.progress = Float(audioPlayer.currentTime/audioPlayer.duration)
+    }
+    
+    @IBAction func btnPauseAudio(_ sender: UIButton) {
+        audioPlayer.pause()
+        setPlayButtons(true, false, true)
+    }
+    
+    @IBAction func btnStopAudio(_ sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0
+        lblCurrentTime.text = convertNSTimeInterval2String(0)
+        setPlayButtons(true, false, false)
+        progressTimer.invalidate()
+    }
+    
+    @IBAction func slChangeVolume(_ sender: UISlider) {
+        audioPlayer.volume = slVolume.value
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        progressTimer.invalidate()
+        setPlayButtons(true, false, false)
+    }
+    
+}
