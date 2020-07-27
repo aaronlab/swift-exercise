abd076d388dc750d2c8061b2ade3499e7de29438
Aaron Lab <aaronlab.net@gmail.com>
2020-07-27 22:14:55 +0900

    enum

--- playground-exercise/Enum.playground/Contents.swift
+++ playground-exercise/Enum.playground/Contents.swift
@@ -34,3 +34,50 @@ var secondDinner: SecondMainDish
 
 secondDinner = SecondMainDish.pasta(taste: PastaTaste.tomato)
 secondDinner = SecondMainDish.pizza(dough: .cheeseCrust, topping: .bacon)
+
+// CaseIterable
+enum School: CaseIterable {
+    case primary, elementary, middle, high, college, university, graduate
+}
+
+let allCases: [School] = School.allCases
+print(allCases)
+
+// with rawValue
+enum SecondSchool: String, CaseIterable {
+    case primary = "유치원"
+    case elementary = "초등학교"
+    case middle = "중학교"
+    case high = "고등학교"
+    case college = "대학"
+    case unitersity = "대학교"
+    case graduate = "대학원"
+}
+
+let secondAllCases: [SecondSchool] = SecondSchool.allCases
+print(secondAllCases)
+
+indirect enum ArithmeticExpression {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let final = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case .number(let value):
+        return value
+    case .addition(let left, let right):
+        return evaluate(left) + evaluate(right)
+    case .multiplication(let left, let right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+let result: Int = evaluate(final)
+print("(5 + 4) * 2 = \(result)")
