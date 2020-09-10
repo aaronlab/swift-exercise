3b190811bc00bf2a4c663e794ad4a604f5716d18
Aaron Lee <aaronlab.net@gmail.com>
2020-09-10 19:13:52 +0900

    Update

---
+++ playground-exercise/Recap.playground/Contents.swift
@@ -0,0 +1,8 @@
+var person: (String, Int, Double) = ("me", 100, 222.2)
+print("who: \(person.0), age: \(person.1), height: \(person.2)")
+
+person.1 = 99
+person.2 = 111.1
+print("who: \(person.0), age: \(person.1), height: \(person.2)")
+
+
