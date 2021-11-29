68b3eae1ef6782c5242978459daa9b09b3a040db
Aaron Lee <aaronlab.net@gmail.com>
2021-11-29 21:04:57 +0900

    init proj

---
+++ MyDiary/MyDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDiary
+//
+//  Created by Aaron Lee on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
